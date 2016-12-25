4568fbb74f91239edb1d1a1077ab1c2dd0915dab
Michelle <jhmichellekim@gmail.com>
2016-12-24 17:57:17 -0800

    debugged removeTransition

--- 01 - JavaScript Drum Kit/play.js
+++ 01 - JavaScript Drum Kit/play.js
@@ -1,23 +1,20 @@
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) {
-    console.log('Not a valid key.')
-    return
-  }; // does not move on to play
-  audio.currentTime = 0; // allows repeating of keys faster than audio length
-  audio.play();
-  key.classList.add('playing');
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        console.log('Not a valid key.')
+        return;
+    }; // does not move on to play
+    audio.currentTime = 0; // allows repeating of keys faster than audio length
+    audio.play();
+    key.classList.add('playing');
 }
 
 function removeTransition(e) {
-  if(e.propertyName !== 'transition') {
-    return; // skip it it's not a transform
-  }
-  this.classList.remove('playing');
+    if (e.propertyName !== 'transform') return; // skip it it's not a transform
+    this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionEnd', removeTransition));
-
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
