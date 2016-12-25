5e86ad39c9129dfa27d124d8a1c1aea9e9dcbe1b
Michelle <jhmichellekim@gmail.com>
2016-12-24 17:08:14 -0800

    added all functions from tutorial

--- 01 - JavaScript Drum Kit/play.js
+++ 01 - JavaScript Drum Kit/play.js
@@ -1,3 +1,23 @@
-window.addEventListener('keydown', function(e) {
-  console.log(e);
-})
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) {
+    console.log('Not a valid key.')
+    return
+  }; // does not move on to play
+  audio.currentTime = 0; // allows repeating of keys faster than audio length
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transition') {
+    return; // skip it it's not a transform
+  }
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionEnd', removeTransition));
+
+window.addEventListener('keydown', playSound);
